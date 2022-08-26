71ca5c70e183c7609f2d4f240212114a014c2522
Matthew Bonig <matthew.bonig@gmail.com>
2022-08-26 07:17:52 -0600

    adding initial webhook code.

---
+++ infrastructure/src/constructs/PackagePublishWebhook/PackagePublishWebhook.lambda.ts
@@ -0,0 +1,124 @@
+import * as child_process from 'child_process';
+import { execSync } from 'child_process';
+import { createWriteStream, writeFileSync } from 'fs';
+import util from 'util';
+import { GetSecretValueCommand, SecretsManagerClient } from '@aws-sdk/client-secrets-manager';
+import axios from 'axios';
+
+
+const exec = util.promisify(child_process.exec);
+
+interface GitHubWebhookEvent {
+  body: string;
+}
+
+interface GithubRelease {
+  release: {
+    name: string;
+    assets_url: string;
+    zipball_url: string;
+    tarball_url: string;
+  };
+}
+
+export const GITHUB_CONFIG_KEY = 'GITHUB_CONFIG_SECRET_ARN';
+export const GITHUB_SSH_PRIVATE_KEY = 'GITHUB_SSH_PRIVATE_KEY_SECRET_ARN';
+export const KNOWN_HOSTS_KEY = 'known_hosts';
+export const ID_RSA_KEY = 'id_rsa';
+export const PAT_KEY = 'pat';
+
+const secretArn = process.env[GITHUB_CONFIG_KEY];
+const privateKeyArn = process.env[GITHUB_SSH_PRIVATE_KEY];
+const client = new SecretsManagerClient({});
+
+let secret: {
+  [PAT_KEY]: string;
+  [KNOWN_HOSTS_KEY]: string;
+  [ID_RSA_KEY]: string;
+};
+
+async function getPat() {
+  const results = await client.send(new GetSecretValueCommand({ SecretId: secretArn }));
+  if (!results.SecretString) {
+    throw new Error('The GitHub Secret has no value');
+  }
+  secret = JSON.parse(results.SecretString);
+  return secret[PAT_KEY];
+}
+
+async function getReleaseAssets(body: GithubRelease) {
+  const pat = await getPat();
+  const response = await axios.get(body.release.tarball_url, {
+    responseType: 'stream',
+    headers: {
+      Authorization: `token ${pat}`,
+    },
+    maxRedirects: 1,
+  });
+  const pipePromise = new Promise((resolve) => {
+    response.data.pipe(createWriteStream('/tmp/package.tar'));
+    response.data.on('end', () => resolve(null));
+  });
+  await pipePromise;
+  const results = await exec('tar -xvf /tmp/package.tar', { cwd: '/tmp/' });
+  const outDir = results.stdout.split('\n')[0];
+  console.log(outDir);
+  return `/tmp/${outDir}`;
+}
+
+async function getPrivateKey() {
+  const results = await client.send(new GetSecretValueCommand({ SecretId: privateKeyArn }));
+  if (!results.SecretString) {
+    throw new Error('The GitHub Private Key Secret has no value');
+  }
+  return results.SecretString!;
+}
+
+async function getRepo() {
+
+  if (!secret) {
+    throw new Error("why don't we have a secret yet?");
+  }
+  const { known_hosts } = secret;
+  const id_rsa = await getPrivateKey();
+  const known_hostsFilename = '/tmp/known_hosts';
+  writeFileSync(known_hostsFilename, known_hosts);
+
+  // Get this from a safe place, say SSM
+  const id_rsaFilename = '/tmp/id_rsa';
+  execSync(`touch ${id_rsaFilename} && rm ${id_rsaFilename}`);
+  writeFileSync(id_rsaFilename, id_rsa, { encoding: 'utf8' });
+  execSync(`chmod 400 ${id_rsaFilename}`, { encoding: 'utf8', stdio: 'inherit' });
+
+  process.env.GIT_SSH_COMMAND = `ssh -o UserKnownHostsFile=${known_hostsFilename} -i ${id_rsaFilename}`;
+
+  const localClone = '/tmp/docs';
+  const remoteRepo = 'git@github.com:monadahq/winglang-docs.git';
+  execSync(`git clone --depth 1 ${remoteRepo} ${localClone}`, { encoding: 'utf8', stdio: 'inherit' });
+
+  execSync(`ls ${localClone}`, { encoding: 'utf8' }).split('\n');
+  return localClone;
+}
+
+function copyDocsOver(releaseNumber: string, releaseAssetDir: string, docsCloneDir: string) {
+  console.log(`going to create a new branch for docs ${releaseNumber}`);
+  const apiDocFilename = `${releaseAssetDir}API.md`;
+  const newDocFilename = `${docsCloneDir}/docs/reference/previous_versions/WingSDK/${releaseNumber.replace('v', '')}.md`;
+  console.log(`Going to copy ${apiDocFilename} to ${newDocFilename}`);
+  // execSync('');
+}
+
+export const handler = async (event: GitHubWebhookEvent) => {
+  if (!secretArn) {
+    throw new Error(`Please provide a GitHub PAT secret on the environment variable ${GITHUB_CONFIG_KEY}`);
+  }
+  // console.log('Event:', JSON.stringify(event, null, 2));
+  const body: GithubRelease = JSON.parse(event.body);
+  // console.log('Body:', JSON.stringify(body, null, 2));
+  const releaseAssetDir = await getReleaseAssets(body);
+
+  const docsCloneDir = await getRepo();
+
+  copyDocsOver(body.release.name, releaseAssetDir, docsCloneDir);
+
+};
---
+++ infrastructure/src/stacks/PackageIngestionStack.ts
@@ -0,0 +1,62 @@
+import { CfnOutput, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { FunctionUrlAuthType, LayerVersion } from 'aws-cdk-lib/aws-lambda';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import { ZipLayer } from 'cdk-lambda-layer-zip';
+import { Construct } from 'constructs';
+import { PackagePublishWebhookFunction } from '../constructs/PackagePublishWebhook/PackagePublishWebhook-function';
+import {
+  GITHUB_CONFIG_KEY,
+  GITHUB_SSH_PRIVATE_KEY,
+  KNOWN_HOSTS_KEY,
+  PAT_KEY,
+} from '../constructs/PackagePublishWebhook/PackagePublishWebhook.lambda';
+
+export class PackageIngestionStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    const sshConfigSecret = new Secret(this, 'SSHConfigSecret', {
+      description: 'A secret to hold some SSH information for git',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({
+          [PAT_KEY]: '',
+          [KNOWN_HOSTS_KEY]: '',
+        }),
+        generateStringKey: PAT_KEY,
+      },
+    });
+
+    const sshSecret = new Secret(this, 'SSHSecret', {
+      description: 'An ssh private key for accessing GitHub',
+
+    });
+
+    // create the handler
+    const packagePublishWebhookFunction = new PackagePublishWebhookFunction(this, 'PackagePublishWebhook', {
+      environment: {
+        [GITHUB_CONFIG_KEY]: sshConfigSecret.secretArn,
+        [GITHUB_SSH_PRIVATE_KEY]: sshSecret.secretArn,
+      },
+      timeout: Duration.minutes(5),
+    });
+
+    sshConfigSecret.grantRead(packagePublishWebhookFunction);
+    sshSecret.grantRead(packagePublishWebhookFunction);
+
+    // add a lambda layer for zip
+    packagePublishWebhookFunction.addLayers(new ZipLayer(this, 'ZipLayer'));
+
+    // add git layer
+    packagePublishWebhookFunction.addLayers(LayerVersion.fromLayerVersionArn(this, 'SSHLayer', 'arn:aws:lambda:us-east-1:553035198032:layer:git-lambda2:8'));
+
+    // set up the functionurl for webhooks
+    const functionUrl = packagePublishWebhookFunction.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
+
+    // what is our webhook url?
+    new CfnOutput(this, 'PackagePublishWebhookUrl', {
+      value: functionUrl.url,
+    });
+  }
+}
