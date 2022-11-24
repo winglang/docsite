dc73f4488dc8f0af267a49cacc52ac137e7c8337
Cristian Pallarés <cristianp@monada.co>
2022-11-24 11:11:54 +0100

    wip

---
+++ scripts/updateDocs.ts
@@ -0,0 +1,51 @@
+import { request } from "@octokit/request";
+import fs from "node:fs/promises";
+import tar from "tar";
+
+const authorization = `token ${process.env.GITHUB_TOKEN}`;
+
+(async () => {
+  console.log("Downloading latest release...");
+  const release = await request("GET /repos/winglang/wing/releases/latest", {
+    headers: {
+      authorization,
+    },
+  });
+  console.log("Release:", release);
+  console.log("Release version:", release.data.tag_name);
+
+  console.log("Looking for the docs.tgz asset...");
+  const asset = release.data.assets.find((asset) => asset.name === "docs.tgz");
+  console.log("Asset:", asset);
+
+  console.log("Downloading latest docs.tgz...");
+  const docsTgz = await request(
+    `GET /repos/winglang/wing/releases/assets/${asset.id}`,
+    {
+      headers: {
+        authorization,
+        accept: "application/octet-stream",
+      },
+    }
+  );
+  await fs.writeFile("docs.tgz", Buffer.from(docsTgz.data));
+
+  console.log("Extracting docs...");
+  await tar.extract({
+    file: "docs.tgz",
+  });
+
+  console.log("Updating versioned_docs/version-latest...");
+  await fs.rm("versioned_docs/version-latest", {
+    force: true,
+    recursive: true,
+  });
+  await fs.rename("docs", "versioned_docs/version-latest");
+
+  console.log("Cleaning up...");
+  await fs.rm("docs.tgz");
+  await fs.rm("docs", {
+    force: true,
+    recursive: true,
+  });
+})();
