winglang/docsite

commit 732d40b0211b3ab97412f516c4434e94f5b1922b

fix: github repository links are 404 (#179)

docusaurus.config.js
@@ -5,7 +5,7 @@ const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const slackUrl = "https://t.winglang.io/slack";
 
-const winglangRepoUrl = "https://github.com/winglang/wing";
+const winglangRepoUrl = "https://github.com/winglang";
 const stackOverflowUrl = "https://stackoverflow.com/questions/tagged/winglang";
 
 /** @type {import('@docusaurus/types').Config} */
@@ -164,23 +164,15 @@ const config = {
             items: [
               {
                 label:"GitHub",
-                href: `${winglangRepoUrl}`
+                href: winglangRepoUrl,
               },
               {
                 label: "Slack",
                 href: slackUrl,
               },
               {
-                label: "GitHub Discussions",
-                href: `${winglangRepoUrl}/discussions`
-              },
-              {
                 label: "Stack Overflow",
                 href: stackOverflowUrl,
-              },
-              {
-                label:"Report an Issue",
-                href: `${winglangRepoUrl}/issues/new/choose`
               },
               {
                 label:"Contributor's Handbook",

commit 5a3ab683cf87b622ce6edd71e69b9d4364ad1129

fix: "edit this page" leads to 404
Fix break caused by #179 where the URLs did not contain the repository URL but rather the Org URL.

docusaurus.config.js
@@ -5,7 +5,7 @@ const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const slackUrl = "https://t.winglang.io/slack";
 
-const winglangRepoUrl = "https://github.com/winglang";
+const winglangOrgUrl = "https://github.com/winglang";
 const stackOverflowUrl = "https://stackoverflow.com/questions/tagged/winglang";
 
 const keywords = [
@@ -81,7 +81,7 @@ const config = {
           routeBasePath: "/", // Serve the docs at the site's root
           breadcrumbs: true,
           includeCurrentVersion: false,
-          editUrl: (params) => `${winglangRepoUrl}/tree/main/docs/${params.docPath}`,
+          editUrl: (params) => `${winglangOrgUrl}/wing/tree/main/docs/${params.docPath}`,
         },
         blog: {
           blogTitle: 'What\'s up? The Wing Blog',
@@ -137,8 +137,8 @@ const config = {
             className: "header-slack-link",
           },
           {
-            href: winglangRepoUrl,
-            "aria-label": "GitHub repository",
+            href: winglangOrgUrl,
+            "aria-label": "GitHub Organization",
             label: " ",
             position: "right",
             className: "header-github-link",