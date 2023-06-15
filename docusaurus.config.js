08799b757361436fd44a0c77d3104547c2f602ae
Sebastian Korfmann <sebastian@korfmann.net>
2023-06-15 13:17:45 +0200

    Checking if that's working on vercel

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
   title: "Wing",
   tagline: "Maximum cloud, minimum DevOps",
   url: process.env.DOCUSAURUS_URL ?? `https://${process.env.VERCEL_URL}`,
-  baseUrl: "/",
+  baseUrl: "/foo/",
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.png",
